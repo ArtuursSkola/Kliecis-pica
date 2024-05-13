1efb21ef2638d4d147151463c52fc9b0249f5f44
ArtuursSkola <arthurswipe0@gmail.com>
2024-05-13 23:23:10 +0300

    Izveidoju pogu, kas ļauj pierakstīties

--- src/Kliecis_piaa.java
+++ src/Kliecis_piaa.java
@@ -1,4 +1,6 @@
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,7 +13,6 @@ public class Kliecis_piaa extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -32,24 +33,44 @@ public class Kliecis_piaa extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(3, 3, 3, 3));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 	    ImageIcon originalImageIcon = new ImageIcon(getClass().getResource("/resources/Picerija.jpeg"));
 	    int scaledWidth = 900;
 	    int scaledHeight = 900;
 	    ImageIcon scaledImageIcon = new ImageIcon(originalImageIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, java.awt.Image.SCALE_SMOOTH));
-	    contentPane.setLayout(null);
-	    
-	    ImageIcon poga = new ImageIcon(getClass().getResource("/resources/red.jpg"));
-	    JButton imageButton = new JButton(poga);
-	    
 	    
 	    JButton btnNewButton = new JButton("New button");
-	    btnNewButton.setBounds(330, 226, 89, 23);
+	    btnNewButton.setSelectedIcon(new ImageIcon(Kliecis_piaa.class.getResource("/resources/tuksa.png")));
+	    btnNewButton.setIcon(new ImageIcon(Kliecis_piaa.class.getResource("/resources/red.jpg")));
+	    btnNewButton.setBounds(281, 179, 210, 63);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Kliecis_pica newWIndow = new Kliecis_pica();
+				newWIndow.setVisible(true);
+				Kliecis_piaa.this.dispose();
+			}
+		});
 	    contentPane.add(btnNewButton);
+	    
+	    JButton btnNewButton1 = new JButton("New button");
+	    btnNewButton1.setSelectedIcon(new ImageIcon(Kliecis_piaa.class.getResource("/resources/tuksa.png")));
+	    btnNewButton1.setIcon(new ImageIcon(Kliecis_piaa.class.getResource("/resources/red.jpg")));
+	    btnNewButton1.setBounds(281, 275, 210, 63);
+	    
+	    contentPane.add(btnNewButton1);
+
+	    
 	    JLabel backroundLabel = new JLabel(scaledImageIcon);
 	    backroundLabel.setBounds(-12, 0, 807, 561);
 	    contentPane.add(backroundLabel);
+	    
+	    
+
+	    
+
 	}
 }
+	
